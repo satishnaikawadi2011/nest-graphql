e5b3066bc8451f4bd758d23869c7c4157b5c5b93
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2021-08-09 20:46:55 +0530

    Successfully added Todos and Users module,service,resolver with cli

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodosModule } from './todos/todos.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports:
+		[
+			TodosModule,
+			GraphQLModule.forRoot({
+				autoSchemaFile: join(process.cwd(), 'src/schema.gql')
+			}),
+			UsersModule
+		],
+	controllers:
+		[
+			AppController
+		],
+	providers:
+		[
+			AppService
+		]
 })
 export class AppModule {}
