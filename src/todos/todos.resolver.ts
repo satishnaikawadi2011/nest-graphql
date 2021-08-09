48ebc685b219f785f0b0ea2b44acbd8d70055761
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2021-08-10 01:50:13 +0530

    Added operations to create Todo and fetch all todos.

--- src/todos/todos.resolver.ts
+++ src/todos/todos.resolver.ts
@@ -1,9 +1,21 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { CreateTodoInput } from './input/create-todo.input';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Todo } from './todo.entity';
+import { TodosService } from './todos.service';
 
 @Resolver()
 export class TodosResolver {
-	@Query(() => String)
-	hello() {
-		return 'Hello World';
+	constructor(private todosService: TodosService) {}
+
+	@Query((returns) => [
+		Todo
+	])
+	getTodos(): Promise<Todo[]> {
+		return this.todosService.findAll();
+	}
+
+	@Mutation((returns) => Todo)
+	createTodo(@Args('createTodoInput') createTodoInput: CreateTodoInput): Promise<Todo> {
+		return this.todosService.create(createTodoInput);
 	}
 }
--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateTodoInput } from './input/create-todo.input';
+import { Todo } from './todo.entity';
 
 @Injectable()
-export class TodosService {}
+export class TodosService {
+	constructor(@InjectRepository(Todo) private repo: Repository<Todo>) {}
+
+	create(createTodoInput: CreateTodoInput) {
+		const todo = this.repo.create(createTodoInput);
+		// todo.user = user;
+		return this.repo.save(todo);
+	}
+
+	findAll() {
+		return this.repo.find();
+	}
+}
